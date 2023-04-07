6561e3dae174a3ff41830d51553577cea16e2690
Guy Cole <guycole@gmail.com>
2023-04-06 17:28:37 -0700

    add dbus experiment

---
+++ dbus/dbus-demo.h
@@ -0,0 +1,12 @@
+/*
+** Title:dbus-demo.h
+**
+** Description:
+**
+** Development Environment:
+**   gcc version 8.3.0 (Debian 8.3.0-6)
+**   BeagleBoard.org Debian Buster IoT Image 2020-04-06
+**
+** Author:
+**   G.S. Cole (guycole at gmail dot com)
+*/
---
+++ dbus/main.c
@@ -0,0 +1,11 @@
+/*
+** Title:main.c
+**
+** Description: dbus demo driver
+** Development Environment:
+**   gcc version 8.3.0 (Debian 8.3.0-6)
+**   BeagleBoard.org Debian Buster IoT Image 2020-04-06
+**
+** Author:
+**   G.S. Cole (guycole at gmail dot com)
+*/
