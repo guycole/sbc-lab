396c5941f4d94042abf99356b8984c3bd83f41e3
Guy Cole <guycole@gmail.com>
2025-02-07 21:21:58 +0000

    working demo

--- rpi/sense_hat/driver.py
+++ rpi/sense_hat/driver.py
@@ -1,7 +1,21 @@
 from sense_hat import SenseHat
 
 if __name__ == '__main__':
-	sense = SenseHat
+        sense = SenseHat()
+        print(sense)
+        print(type(sense))
+
+        sense.show_message("192.168.5.8")
+        sense.show_letter("z")
+
+        temperature = sense.get_temperature()
+        print(f"temperature:{temperature}")
+
+        temperature = sense.get_temperature_from_humidity()
+        print(f"temperature:{temperature}")
+
+        temperature = sense.get_temperature_from_pressure()
+        print(f"temperature:{temperature}")
 
 #;;; Local Variables: ***
 #;;; mode:python ***
